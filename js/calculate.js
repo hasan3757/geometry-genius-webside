7208ac15177a4d679dae895528ec0105d731f2f6
hasan3757 <hasanmir3757@gmail.com>
2024-02-17 17:20:20 +0600

    second commot

--- js/calculate.js
+++ js/calculate.js
@@ -1,3 +1,4 @@
+// trigle
 function calculateTrigleArea(){
     // base
     const triangleBaseInput = document.getElementById('triangle-base');
@@ -18,4 +19,27 @@ function calculateTrigleArea(){
     // display area
     const triangleAreaSpan = document.getElementById('triangle-area');
     triangleAreaSpan.innerText = area;
+}
+
+// rectangle
+function calculateRectangleArea(){
+    // base2
+    const rectangleBaseInput = document.getElementById('rectangle-base');
+    const rectangleBaseTest = rectangleBaseInput.value;
+    const base2 = parseFloat(rectangleBaseTest);
+    console.log(base2);
+
+    // height2
+    const rectangleHeightInput = document.getElementById('rectangle-height');
+    const rectangleHeightText = rectangleHeightInput.value;
+    const height2 = parseFloat(rectangleHeightText);
+    console.log(height2);
+
+    // calculate area2
+    const area2 = base2 * height2
+    console.log(area2);
+
+    // display area2
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area2;
 }
